00ac48bf024ac23f01aee2df9141496ad93f97ee
MORESUCCESS <130167357+MORESUCCESS@users.noreply.github.com>
2024-01-20 23:32:52 +0100

    Add files via upload

---
+++ assets/loginReg.js
@@ -0,0 +1,15 @@
+
+
+
+// ..................Start Form Registration Popup.........
+document.querySelector('#popRegForm').addEventListener('click', ()=>{
+    document.querySelector('.form-registration').style.display='flex'
+})
+
+
+// ..................Close IconBtn.........................
+document.querySelectorAll('.close').forEach(AllCloser=>{
+    AllCloser.addEventListener('click', ()=>{
+        document.querySelector('.form-registration').style.display='none'
+    })
+})
